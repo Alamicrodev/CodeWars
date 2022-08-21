0c96a670295aa28453a9aeca263a11fc15f652ec
Moheud Malic <mohidalamsohail22@live.com>
2022-08-21 20:20:03 +0300

    1 new challenge

---
+++ Codewars/7kyu/binary_sCore.js
@@ -0,0 +1,24 @@
+// Given a number n we will define its scORe to be 0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+
+// Write a function that takes n and finds it's scORe.
+
+// n	scORe n
+// 0	0
+// 1	1
+// 49	63
+// 1000000	1048575
+
+// Any feedback would be much appreciated
+
+// -------------------- My Solution -----------------------------------
+
+
+function score(n){
+    let orReturn = 0; 
+    for (let i = 0; i <= n; i++)
+      {
+        orReturn = orReturn | i 
+      }
+  
+    return orReturn
+  }
