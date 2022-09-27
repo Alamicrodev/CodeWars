81ac795934095979574f712d68481f4efc87588c
Moheud Malic <mohidalamsohail22@live.com>
2022-09-27 23:30:17 +0300

    split string into a pairs of two chars, if odd number of chars add _

---
+++ 6kyu/split_strings.js
@@ -0,0 +1,23 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+// -------------------- My Solution -----------------------------------
+
+function solution(str){
+    if (str.length % 2 != 0) {
+      str = str + "_"
+    }
+   
+   let pairs = []
+   
+   for (let i = 0; i < str.length; i+=2)
+ {
+   pairs.push(str[i]+str[i+1])
+ }  
+   
+   return pairs
+}
