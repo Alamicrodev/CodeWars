b9ed212fca0de41bc015a847ca59289ebc77980e
Moheud Malic <mohidalamsohail22@live.com>
2022-10-09 23:29:58 +0300

    check if the given year is a leap year on an alien planet

---
+++ 7kyu/leap_year_on_an_alien_planet.js
@@ -0,0 +1,36 @@
+// Our spaceship has crashed on an unknown planet many light years away from earth. 
+// Thankfully we were able to send out a distress signal right before the crash. 
+// Help will be here shortly but we need to gather as much information about this planet as we can before we're rescued.
+
+// Before our control panels were destroyed, we were able to gather the duration of this planet's orbit around it's planetary system's star.
+
+// Among other things, we need to determine if a given year is a leap year on this planet.
+
+// Your Task:
+// Given the duration of the planet's orbit (in days) and a specific year on this planet, determine if the given year is a leap year here.
+
+// For example:
+// On Earth, a single rotation around the sun takes 365.25 days. 
+// Therefore, each year takes 365 days but every forth year is a leap year and takes 366 days. 
+// The next leap year on Earth will occur in 2020.
+
+// -------------------- My Solution -----------------------------------
+
+function isLeapYear(duration, year) {
+    let afterDecimal = Number("0."+String(duration).split('.')[1])
+    
+    console.log(afterDecimal)
+ 
+    let leapAfterYears = Math.round(1 / afterDecimal) 
+ 
+    console.log(leapAfterYears)
+    
+    if (year % leapAfterYears == 0) {
+      return true
+    }
+   else 
+     {
+       return false 
+     }
+
+}
