783bd56a54c33e2ade1614f46578a6bcb3162b60
Moheud Malic <mohidalamsohail22@live.com>
2022-10-20 23:34:31 +0300

    check if the number is narcisstic

--- 6kyu/does_my_number_look_big_in_this.js
+++ 6kyu/does_my_number_look_big_in_this.js
@@ -12,7 +12,7 @@
 // Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
 //  This may be True and False in your language, e.g. PHP.
 
-// Error checking for text strings or other invalid inputs is not required,
+// Error checking for text strings or other invalid inputs is not required, 
 //  only valid positive non-zero integers will be passed into the function.
 
 // -------------------- My Solution -----------------------------------
