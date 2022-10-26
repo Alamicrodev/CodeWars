af275887ff4cb3f472a9652bb56e7e9de4dafb14
Moheud Malic <mohidalamsohail22@live.com>
2022-10-26 23:45:13 +0300

    lol challenge double a number

---
+++ 8kyu/you_cant_code_under_pressure.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+// LMAO THIS HAS TO BE A JOKE BUT IT WAS ACTUALLY A CHALLENGE 
+
+// -------------------- My Solution -----------------------------------
+
+function doubleInteger(i) {
+    return i*2;
+    //lol
+  }
