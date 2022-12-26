8e6d7d3f873eec6d45bac4638495a5b32bd919b7
Moheud Malic <mohidalamsohail22@live.com>
2022-12-26 10:36:23 +0300

    the christmas sorter

---
+++ 7kyu/the_12_days_of_christmas.js
@@ -0,0 +1,29 @@
+// Oh No!
+// The song sheets have been dropped in the snow and the lines of each verse have become all jumbled up.
+
+// Task
+// You have to write a comparator function which can sort the lines back into their correct order, otherwise Christmas will be cancelled!
+
+// Reminder: Below is what the final verse should look like
+
+// On the 12th day of Christmas my true love gave to me
+// 12 drummers drumming,
+// 11 pipers piping, 
+// 10 lords a leaping, 
+// 9 ladies dancing, 
+// 8 maids a milking,
+// 7 swans a swimming, 
+// 6 geese a laying, 
+// 5 golden rings, 
+// 4 calling birds,
+// 3 French hens, 
+// 2 turtle doves and 
+// a partridge in a pear tree.
+
+// -------------------- My Solution -----------------------------------
+
+var comparator = function(a,b) {
+    let indexArray = ["On", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "a"]
+    
+    return indexArray.indexOf(a.split(" ")[0]) - indexArray.indexOf(b.split(" ")[0])
+  }
